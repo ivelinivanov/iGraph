625100708cc14d467e884e0ffe93b8ebf6fd3c26
ivelinivanov <ivelin.ivanov@mentormate.com>
2013-09-04 16:56:00 +0300

    Initial Commit

---
+++ iGraph/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iGraph
+//
+//  Created by Ivelin Ivanov on 9/4/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iGraph/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iGraph
+//
+//  Created by Ivelin Ivanov on 9/4/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
